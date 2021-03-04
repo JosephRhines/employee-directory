666ff23c10a34f957c06272d1630397d840aa5b4
Joseph Rhines <jprhines@gmail.com>
2021-03-04 09:43:37 -0600

    I created an EmployeeOrg function that contains the grid and props to organize the employees

---
+++ src/components/EmployeeOrg.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+function EmployeeOrg (props) {
+    return (
+        <div className ="container">
+          <div className="row">
+              <div className="col-md-2">
+                  <p><img src={props.thumbnail} className="thumbnail"></img></p>
+              </div>
+              <div className="col-md-2">
+                  <p>{props.firstName} {props.lastName}</p>
+              </div>
+              <div className="col-md-2">
+                  <p>{props.phone}</p>
+              </div>
+              <div className="col-md-4">
+                  <p>{props.email}</p>
+              </div>
+              <div className="col-md-2">
+                  <p>{props.age}</p>
+              </div>
+          </div>
+          <hr></hr>
+        </div>
+              
+
+    )
+}
+
+export default EmployeeOrg;
