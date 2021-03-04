84c7e94ada18fcdb7480d6b4946e2f17feeb62f6
Joseph Rhines <jprhines@gmail.com>
2021-03-04 15:48:45 -0600

    I put the title code from EmployeeOrg and placed it in this component so the titles wouldn't repeat on each employee

---
+++ src/components/Titles.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Titles(){
+ return(
+
+<div className="row">
+                <div className="col-md-2">
+                    <p><strong>Image</strong></p>
+                </div>
+              <div className="col-md-2">
+                  <p><strong>Name</strong></p>
+              </div>
+              <div className="col-md-2">
+                  <p><strong>Phone</strong></p>
+              </div>
+              <div className="col-md-4">
+                  <p><strong>email</strong></p>
+              </div>
+              <div className="col-md-2">
+                  <p><strong>Age</strong></p>
+              </div>
+
+            </div>
+ )}
+export default Titles;
