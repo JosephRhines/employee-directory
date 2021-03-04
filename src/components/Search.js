883fdfc4265471035757fd545c8dee46410035e4
Joseph Rhines <jprhines@gmail.com>
2021-03-03 22:17:36 -0600

    I added an onChange and value to the input field and an onClick to the button

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,19 @@ import React from "react"
 
 
 
-function Search(){
+function Search(props){
 return (
 <div><form className="d-flex">
-<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-<button className="btn btn-outline-success" type="submit">Search</button>
+<input className="form-control me-2" 
+       type="search" 
+       placeholder="Search" 
+       aria-label="Search"
+       onChange={props.handleInputChange}
+       value={props.value}
+       name="search"
+       
+       />
+<button onClick={props.handleFormSubmit}className="btn btn-outline-success" type="submit">Search</button>
 </form> 
 </div>
 
